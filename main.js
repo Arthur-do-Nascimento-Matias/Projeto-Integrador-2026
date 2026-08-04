a6967fde5b379e3c2d0a283097c4f11f66fc1509
ARTHUR DO NASCIMENTO MATIAS <arthur.matias@alunos.sc.senac.br>
2026-08-04 10:35:59 -0300

    'v1'

--- main.js
+++ main.js
@@ -9,12 +9,20 @@ const atividades = [
 const trilha = document.querySelector('.trilha')
 const telaAtividade = document.querySelector('.atividade')
 const enunciado = document.querySelector('.enunciado')
+const burguer = document.querySelector('#burger')
+const menu = document.querySelector('.menuLateralEsquerda')
 
 for(let i=0; i < atividades.length; i++){
     const botao = document.createElement('button')
     botao.className = 'botaoAtividade'
     botao.textContent = atividades[i]
     botao.id = i+1
+    if(i % 2 == 0) {
+        botao.classList.add('impar')
+    }
+    else{
+        botao.classList.add('par')
+    }
     botao.addEventListener('click', () => entrarAtividade(botao.id))
     trilha.appendChild(botao)
 }
@@ -33,3 +41,12 @@ function sairAtividade() {
     telaAtividade.style.transform = 'translateX(100%)'
     trilha.style.opacity = '1'
 }
+
+burguer.addEventListener('change', () => {
+    if(burguer.checked) {
+    menu.classList.add('ativo')
+} else {
+    menu.classList.remove('ativo')
+}
+
+})
