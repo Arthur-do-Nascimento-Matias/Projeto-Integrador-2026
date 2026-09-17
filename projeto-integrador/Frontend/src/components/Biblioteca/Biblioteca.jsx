f3ceb537ba7d8bfec27d381ed076667fca52cd97
Arthur-do-Nascimento-Matias <arthurdonascimentomatias23@gmail.com>
2026-09-17 08:34:59 -0300

    refactor: chatBot, selecionar materias, bibliotecas

--- projeto-integrador/Frontend/src/components/Biblioteca/Biblioteca.jsx
+++ projeto-integrador/Frontend/src/components/Biblioteca/Biblioteca.jsx
@@ -1,47 +1,58 @@
 import { useState, useEffect } from "react";
-import '../Biblioteca/Biblioteca.css'
+import "../Biblioteca/Biblioteca.css";
 
 function Biblioteca() {
 
     const [capas, setCapas] = useState([]);
-    
+
     useEffect(() => {
 
-            fetch(`http://localhost:5000/livros`)
+        fetch("http://localhost:5000/livros")
             .then(resp => resp.json())
             .then(data => {
+                console.log(data);
+                setCapas(data);
+            });
 
-                console.log(data)
-                setCapas(data)
+        console.log("biblioteca");
 
-                }
-            )
+    }, []);
 
-                console.log('biblioteca')
-
-        }, [])
-
-    return(
+    return (
         <>
-        <div className="capas">
+            <div className="container">
 
-                    {capas.map((capa, index) => {
-                        return(
-                        <img
-                            id={capa.id}
-                            className="capa"
-                            key={index}
-                            src={`data:image/jpeg;base64,${capa.img}`}
-                            alt={`Capa ${index + 1}`}
-                            width="200"
-                            onClick={() => window.open(`http://localhost:5000/abrirLivro?id=${capa.id}`)}
-                        />
-                        )
+                <div className="capas">
+
+                    {capas.map((capa) => {
+
+                        return (
+                            <div
+                                className="livro"
+                                key={capa.id}
+                            >
+                                <img
+                                    id={capa.id}
+                                    className="capa"
+                                    src={`data:image/jpeg;base64,${capa.img}`}
+                                    alt={`Capa do livro ${capa.id}`}
+                                    onClick={() =>
+                                        window.open(
+                                            `http://localhost:5000/abrirLivro?id=${capa.id}`,
+                                            "_blank"
+                                        )
+                                    }
+                                />
+                            </div>
+                        );
+
                     })}
+
+                </div>
 
             </div>
         </>
-    )
+    );
 }
 
-export default Biblioteca
+export default Biblioteca;  
